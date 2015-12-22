e0f72b710b921ba69a2fd3201cf75f012df82d0f
gitxiaowen <gitxiaowen@gmail.com>
2015-12-22 17:05:02 +0800

    新增文章分页显示的类

---
+++ blog/src/com/blog/dao/ArticleDao.java
@@ -0,0 +1,25 @@
+package com.blog.dao;
+import java.util.List;
+import com.blog.entity.*;
+
+/**
+ * 类标识：文章相关的数据操作类
+ * 
+ * 创建时间：2015年12月22日17:04:13
+ * 
+ * @author XiaoWen
+ *
+ */
+public class ArticleDao {
+
+
+	public List<Article> allArticle()
+	{
+		
+		
+		
+		return null;
+	}
+	
+	
+}
