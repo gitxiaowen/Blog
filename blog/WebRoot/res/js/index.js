141238f709c9ee6e14b4b123448ff75a6749af20
gitxiaowen <gitxiaowen@gmail.com>
2015-12-23 09:32:47 +0800

    完成静态页面之间的跳转

--- blog/WebRoot/res/js/index.js
+++ blog/WebRoot/res/js/index.js
@@ -1,3 +1,25 @@
 /**
+ * 代码描述：主界面的跳转
  * 
+ * 创建时间：2015年12月23日09:07:19
+ * 
+ * 创建人：熊小文
  */
+$(document).ready(function() {
+	$(".overlay").css('display','none');
+	$(".login-reg").click(function() {
+		$('.overlay').css('display','block');
+		$('.overlay-login').css('z-index','999');
+		$('.overlay-mailsent').css('z-index','1');
+		console.log("显示登录注册界面");
+	});
+	$(".close").click(function(){
+		$(".overlay").css('display','none');
+	});
+	$(".login").click(function(){
+		$(".overlay").css('display','block');
+		$('.overlay-mailsent').css('z-index','999');
+		$('.overlay-login').css('z-index','1');
+	})
+	
+});
