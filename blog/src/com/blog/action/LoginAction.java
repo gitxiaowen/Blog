2f7e9a392e23637ce9995bd34cc0e9af33f79183
gitxiaowen <gitxiaowen@gmail.com>
2015-12-24 21:54:44 +0800

    登录Action

---
+++ blog/src/com/blog/action/LoginAction.java
@@ -0,0 +1,61 @@
+package com.blog.action;
+
+
+import java.util.Map;
+
+import com.blog.dao.LoginDao;
+import com.blog.dao.UserDao;
+import com.blog.entity.User;
+import com.blog.utils.Email;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+
+
+/*******************************************
+ * 	创建标识： 熊小文   2015.10.14
+ * 
+ * 	类描述：登录操作类
+ * 
+ * 	修改内容：
+ * 	修改时间：
+ * 	修改原因：
+ *******************************************/
+public class LoginAction extends ActionSupport {
+	private String email;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		//根据邮箱该用户已注册过还是新用户
+		LoginDao ld=new LoginDao();
+		System.out.println(getEmail());
+		//如果为新用户，发送创建账户的邮件，并插入数据库中
+		if(!LoginDao.isNewUser(getEmail()))
+		{
+			//发送邮件
+			System.out.println("进入");
+//			Email.sendMail(getEmail(), "");
+			//注册此账户
+			UserDao ud=new UserDao();
+			ud.RegUser(getEmail());		
+		}
+		else
+		{
+			
+		}
+		return "success";
+	}
+
+	
+
+}
